af6d9aea6e0e2df74979ef5a3a58978e89c11e65
fouzia-sarker <122530618+fouzia-sarker@users.noreply.github.com>
2023-12-12 00:19:30 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank Management</title>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" href="bootstrap/css//bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="index.php">Blood Bank Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin/login.php">Admin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="donor/login.php">Donor</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="patient/login.php">Patient</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+<!-- Main content -->
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 m-auto" style="text-align: justify;">
+            <h4>What is Blood Bank Management System?</h4>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, corrupti fugiat alias voluptatum, cupiditate dolorem fuga laborum perferendis eos laboriosam animi ullam libero doloremque accusamus optio, laudantium nesciunt exercitationem nam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, corrupti fugiat alias voluptatum, cupiditate dolorem fuga laborum perferendis eos laboriosam animi ullam libero doloremque accusamus optio, laudantium nesciunt exercitationem nam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, corrupti fugiat alias voluptatum, cupiditate dolorem fuga laborum perferendis eos laboriosam animi ullam libero doloremque accusamus optio, laudantium nesciunt exercitationem nam.
+            </p>
+            <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, corrupti fugiat alias voluptatum, cupiditate dolorem fuga laborum perferendis eos laboriosam animi ullam libero doloremque accusamus optio, laudantium nesciunt exercitationem nam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, corrupti fugiat alias voluptatum, cupiditate dolorem fuga laborum perferendis eos laboriosam animi ullam libero doloremque accusamus optio, laudantium nesciunt exercitationem nam.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+    </div>
+</div>
+</body>
+</html>
